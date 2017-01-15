dc4d4fbf897e81270b401ddc36da5ddabd3688c9
TodorBalabanov <todor.balabanov@gmail.com>
2017-01-15 13:55:27 +0200

    Results from the first GA implementation will be used for the article.

--- src/eu/veldsoft/esgi120/p2/Main.java
+++ src/eu/veldsoft/esgi120/p2/Main.java
@@ -117,7 +117,7 @@ public class Main {
 		// TODO Do profiling to find the bottle necks in the code.
 		// TODO Write LSSC 2017 paper.
 
-		// optimization1(X, Y, pieces);
-		optimization2(X, Y, pieces);
+		optimization1(X, Y, pieces);
+		// optimization2(X, Y, pieces);
 	}
 }
